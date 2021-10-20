f85add99d3f94d86ac1ba88cb8b54d30ed12dff6
Scott <hscottvo@gmail.com>
2021-10-20 13:48:15 -0700

    Adjusted Game - Allows multiple square sizes - prints more intelligently - Adds string converter to hash into dupe-table - Allows copies for branching

--- game.py
+++ game.py
@@ -1,35 +1,37 @@
-from enum import Enum
-
-class Direction(Enum): 
-    DOWN = (1, 0)
-    UP = (-1, 0)
-    RIGHT = (0, 1)
-    LEFT = (0, -1)
+from util import *
+from copy import deepcopy
 
 
 class Game:
     def __init__(self, start): 
         self.game_state = start
-        self.coords = [-1, -1]
-        for i in range(len(start)):
-            for j in range(len(start)):
-                if start[i][j] == '\u25a1':
-                    self.coords = [i, j]
+        self.coords = []
+        self.__init_coords()
         self.size = len(start[0])
         # self.solution = [str(i) for i in range(1, self.size+1)]
         self.solution = []
         self.__init_solution()
+        self.max_val = self.size ** 2 - 1
        
 
     def __init_solution(self):
         for i in range(1, self.size+1):
             self.solution.append([str(j + self.size*(i-1)) for j in range(1, self.size+1)]) 
-        self.solution[self.size-1][self.size-1] = '\u25a1'  
+        self.solution[self.size-1][self.size-1] = '\u25a1' 
+
+
+    def __init_coords(self):
+        for i in range(len(self.game_state)):
+            for j in range(len(self.game_state)):
+                if self.game_state[i][j] == '\u25a1':
+                    self.coords = [i, j]
 
     
     def print(self, debug: bool = False): 
         for i in self.game_state:
-            print(i[0], i[1], i[2], sep=' ', end='\n')
+            for j in i: 
+                print(j + ' '*((self.max_val // 10 + 2) - len(j)), end='')
+            print('')
         print('')
         if debug: 
             print("Empty space is in: Row " + str(self.coords[0]), "Column " + str(self.coords[1]), sep='  ')
@@ -47,10 +49,49 @@ class Game:
         self.__switch(self.coords, [sum(x) for x in zip(self.coords, dir.value)])
         pass
 
+
+    def copy_move(self, dir:Enum):
+        new_game = Game(self.__copy_string())
+        new_game.move(dir)
+        return new_game
+
+
     def __switch(self, coords_a, coords_b):
         self.game_state[coords_a[0]][coords_a[1]], self.game_state[coords_b[0]][coords_b[1]] = \
             self.game_state[coords_b[0]][coords_b[1]], self.game_state[coords_a[0]][coords_a[1]]
         self.coords = coords_b
+    
+
+    def __copy_string(self):
+        return [i.copy() for i in self.game_state]
+
+
+    def manhattan_heuristic(self):
+        ret = 0
+        for i, row in enumerate(self.game_state):
+            for j, item in enumerate(row):
+                if item != '\u25a1':
+                    curr_pos = (i, j)
+                    item_num = int(item)
+                    sol_pos = ((item_num-1)//self.size, (item_num-1)%self.size)
+                    ret += manhattan_dist(sol_pos, curr_pos)
+        return ret
+
+
+    def misplaced_tile_hueristic(self):
+        ret = 0
+        for i, row in enumerate(self.game_state):
+            for j, item in enumerate(row):
+                if item != '\u25a1':
+                    sol_val = i * self.size + j + 1
+                    curr_val = int(item)
+                    if sol_val != curr_val:
+                        ret += 1
+        return ret
+
+
+    def string(self):
+        return ' '.join([i for i in [' '.join(j) for j in self.game_state]])
 
 
     def check_solution(self):
@@ -58,14 +99,14 @@ class Game:
 
 
 if __name__ == "__main__": 
-    user_input = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '\u25a1', '11', '12'], ['13', '14', '15', '16']]
+    user_input = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['13', '14', '15', '\u25a1']]
     x = Game(user_input)
-    x.print()
+    # y = x.game_state.deepcopy()
     x.move(Direction.LEFT)
+    x.move(Direction.LEFT)
+    y = x.copy_move(Direction.RIGHT)
     x.print()
-    x.move(Direction.UP)
-    x.print()
-    print(x.check_solution())
-    print(x.solution)
-    print(x.game_state)
+    y.print()
+    print(x.string())
+
     
