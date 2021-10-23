959cb0d0488be228152b503f73418341fc1c3255
Scott <hscottvo@gmail.com>
2021-10-23 03:20:43 -0700

    Added contains_key to util.py - Returns true if key already exists in dictionary (in this case, if that particular game state has been visited already - If the game state has not been visited, set that value to 1 and return False

--- util.py
+++ util.py
@@ -4,6 +4,14 @@ def manhattan_dist(point_a, point_b):
     return (abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1]))
 
 
+def contains_key(dict: dictionary, key: str):
+    if key in dict:
+        return True
+    else:
+        dict[key] = 1
+        return False
+
+
 class Direction(Enum): 
     DOWN = (1, 0)
     UP = (-1, 0)
