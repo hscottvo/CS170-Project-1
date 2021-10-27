698278571b93fdf9c869b89ddae28de72a3778f8
Scott <hscottvo@gmail.com>
2021-10-26 21:01:43 -0700

    Removed dictionary for dupes (using sets instead)

--- util.py
+++ util.py
@@ -4,15 +4,6 @@ def manhattan_dist(point_a, point_b):
     return (abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1]))
 
 
-def contains_key(dict: dictionary, key: str):
-    # TODO: Try using sets instead of dictionaries, since we don't actually use the values at all
-    if key in dict:
-        return True
-    else:
-        dict[key] = 1
-        return False
-
-
 class Direction(Enum): 
     DOWN = (1, 0)
     UP = (-1, 0)
