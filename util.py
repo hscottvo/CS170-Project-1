eeefc92953d8b1f9ab2867550403cfbf4a55d99a
Scott <hscottvo@gmail.com>
2021-10-26 22:37:41 -0700

    Implemented dataclass for priority queue

--- util.py
+++ util.py
@@ -1,7 +1,18 @@
+from dataclasses import dataclass, field
+from typing import Any
 from enum import Enum
 
 def manhattan_dist(point_a, point_b):
     return (abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1]))
+
+
+@dataclass(order=True)
+class PrioGame:
+    priority: int
+    item: Any=field(compare=False)
+
+    def get_item(self):
+        return self.item
 
 
 class Direction(Enum): 
@@ -9,3 +20,6 @@ class Direction(Enum):
     UP = (-1, 0)
     RIGHT = (0, 1)
     LEFT = (0, -1)
+
+if __name__ == '__main__':
+    pass
