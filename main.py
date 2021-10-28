1589da66ced739e2b87f7389f9e5b0c6cbfb3dee
Scott <hscottvo@gmail.com>
2021-10-27 22:32:09 -0700

    Recorded times for different depths

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from game import Game
 from queue import Queue, PriorityQueue
-from heapq import heapify, heappush, heappop
 from util import Direction, PrioGame
 from time import time
 
@@ -87,41 +86,39 @@ def manhattan_search(game: Game):
 
 if __name__=='__main__':
 
-    # depth 0: uniform (0.0003 seconds)
+    # depth 0: uniform (0.0003 seconds), misplaced_tile (< 0.001 seconds), manhattan (< 0.001 seconds)
     # game = Game([['1', '2', '3'], ['4', '5', '6'], ['7', '8', '\u25a1']], empty='\u25a1')
 
-    # depth 1: uniform (0.001 seconds)
+    # depth 1: uniform (0.001 seconds), misplaced_tile (< 0.001 seconds), manhattan (< 0.001 seconds)
     # game = Game([['1', '2', '3'], ['4', '5', '6'], ['7', '\u25a1', '8']], empty='\u25a1')
 
-    # depth 2: uniform (0.007 seconds)
+    # depth 2: uniform (0.007 seconds), misplaced_tile (< 0.001 seconds), manhattan (< 0.001 seconds)
     # game = Game([['1', '2', '3'], ['4', '5', '6'], ['\u25a1', '7', '8']], empty='\u25a1')
 
-    # depth 4: uniform (0.011 seconds)
+    # depth 4: uniform (0.011 seconds), misplaced_tile (0.001 seconds), manhattan (0.001 seconds)
     # game = Game([['1', '2', '3'], ['5', '\u25a1', '6'], ['4', '7', '8']], empty='\u25a1')
 
-    # depth 8: uniform (0.051 seconds)
+    # depth 8: uniform (0.051 seconds), misplaced_tile (0.002 seconds), manhattan (0.001 seconds)
     # game = Game([['1', '3', '6'], ['5', '\u25a1', '2'], ['4', '7', '8']], empty='\u25a1')
 
-    # depth 12: uniform (0.402 seconds)
+    # depth 12: uniform (0.402 seconds), misplaced_tile (.008 seconds), manhattan (0.004 seconds)
     # game = Game([['1', '3', '6'], ['5', '\u25a1', '7'], ['4', '8', '2']], empty='\u25a1')
 
-    # depth 16: uniform (2.66 seconds)
+    # depth 16: uniform (2.66 seconds), misplaced_tile (0.07 seconds), manhattan (0.019 seconds)
     # game = Game([['1', '6', '7'], ['5', '\u25a1', '3'], ['4', '8', '2']], empty='\u25a1')
 
-    # depth 20: uniform(3.705 seconds)
+    # depth 20: uniform(3.705 seconds), misplaced_tile (0.182 seconds), manhattan (0.033 seconds)
     # game = Game([['7', '1', '2'], ['4', '8', '5'], ['6', '3', '\u25a1']], empty='\u25a1')
 
-    # depth 24: uniform(17.388 seconds)
+    # depth 24: uniform(17.388 seconds), misplaced_tile (1.145 seconds), manhattan (0.129 seconds)
     game = Game([['\u25a1', '7', '2'], ['4', '6', '1'], ['3', '5', '8']], empty='\u25a1')
 
     
     start_time = time()
-    solved_game = uniform_cost_search(game)
-    p1 = Process(target=uniform_cost_search, args=(game,))
-    p1.start()
 
+    # solved_game = uniform_cost_search(game)
     # solved_game = misplaced_tile_search(game)
-    # solved_game = manhattan_search(game)
+    solved_game = manhattan_search(game)
     run_time = time() - start_time
     solved_game.print_path()
     
